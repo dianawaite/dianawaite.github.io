ddbbfad9385449ba40e7da1e6fd0f9443019eeab
dianawaite <wai19007@byui.edu>
2020-04-25 18:15:02 -0600

    a few final updates

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
 try {
   let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
     year: "numeric"
   };
-  document.getElementById("currentdate").textContent = new Date().toLocaleDateString("en-US", options);
+  document.getElementById("currentdate").innerHTML = new Date().toLocaleDateString("en-US", options);
 } catch (e) {
   alert("Error with code or your browser does not support Locale");
 }
