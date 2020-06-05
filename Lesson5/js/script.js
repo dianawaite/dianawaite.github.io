340c39c8342a80fe97309b8dbc8a5d161bda39c1
dianawaite <wai19007@byui.edu>
2020-06-04 18:45:57 -0600

    w5 updates - date, favicon

--- Lesson5/js/script.js
+++ Lesson5/js/script.js
@@ -3,9 +3,23 @@ const mainnav = document.querySelector('.navigation');
 
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 
-n = new Date();
-y = n.getFullYear();
-m = n.getMonth();
-d = n.getDate();
-a = n.getDay();
-document.getElementById("date").innerHTML = a + ", " + d +" " + m + " " + y;
+
+{
+let today = new Date();
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let months = 
+["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let date = days[today.getDay()] + ', ' + today.getDate() + " " + months[today.getMonth()] + " " + today.getFullYear();
+document.getElementById("date").innerHTML = date;}
+
+
+
+//notice
+let d = new Date();
+let dayOfWeek = d.getDay();
+
+if (dayOfWeek == 5) {
+  document.getElementById('notice').style.display = 'block';
+} else {
+  document.getElementById('notice').style.display = 'none';
+}
