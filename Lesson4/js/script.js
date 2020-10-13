0c73756d3238105ba3092551146b975bbf7b4bcc
dianawaite <wai19007@byui.edu>
2020-10-12 20:25:19 -0600

    css changes preston town page

--- Lesson4/js/script.js
+++ Lesson4/js/script.js
@@ -1,8 +1,3 @@
-const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('.navigation')
-
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
-
 function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
