f499a75405b74023fcf37f27d2ad0efef0c84bbb
dianawaite <wai19007@byui.edu>
2020-10-12 21:26:16 -0600

    navigation fixes Lesson 4

--- Lesson4/js/script.js
+++ Lesson4/js/script.js
@@ -1,3 +1,19 @@
 function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
+
+const months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
+let currDate = new Date();
+let currYear = currDate.getFullYear();   
+
+let lastUpdate = new Date(document.lastModified);
+let day = days[lastUpdate.getDay()];
+let date = lastUpdate.getDate();
+let month = months[lastUpdate.getMonth()];
+let year = lastUpdate.getFullYear();         
+let lastUpdatedTxt = "Last Updated: " + day + ", " + date + " " + month + " " + year + ".";
+
+document.getElementById('currYear').textContent = currYear;
+document.getElementById('lastUpdated').innerHTML = lastUpdatedTxt;
