1df6bcbdb9291e99ba90dee9516c21142f0b09f6
dianawaite <wai19007@byui.edu>
2020-10-25 19:11:33 -0600

    Lesson6 updates

--- main.js
+++ main.js
@@ -3,4 +3,11 @@
     year: "numeric"
   };
   document.getElementById("currentdate").innerHTML = new Date().toLocaleDateString("en-US", options);
-} 
+
+WebFont.load({
+  google: {
+    families: [
+       'Roboto Condensed', sans-serif
+    ]
+  }
+});
